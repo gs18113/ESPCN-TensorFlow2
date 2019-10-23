fd1dea512e93dc61a3fee5ec11dc789f770154fa
gs18113 <seungmin960@gmail.com>
2019-10-23 22:13:34 +0900

    init

---
+++ model.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow import keras
+
+class ESPCN(keras.Model):
+    def __init__(self, upscale_factor):
+        self.conv1 = keras.layers.Conv2D(64, 5, padding='same', activation='tanh', kernel_initializer='orthogonal')
+        self.conv2 = keras.layers.Conv2D(64, 3, padding='same', activation='tanh', kernel_initializer='orthogonal')
+        self.conv3 = keras.layers.Conv2D(64, 3, padding='same', activation='tanh', kernel_initializer='orthogonal')
+        self.conv4 = keras.layers.Conv2D((upscale_factor ** 2), 3, padding='same', activation='tanh', kernel_initializer='orthogonal')
+        self.upscale_factor = upscale_factor
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = tf.nn.depth_to_space(x, self.upscale_factor)
+        return x
