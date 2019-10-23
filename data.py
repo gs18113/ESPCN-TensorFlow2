61581ce43e04eaa2ab1689f4bf1696a740d44f1c
gs18113 <seungmin960@gmail.com>
2019-10-23 23:06:55 +0900

    complete

--- data.py
+++ data.py
@@ -4,7 +4,6 @@ from six.moves import urllib
 import tarfile
 
 import tensorflow as tf
-from tensorflow.data import Dataset
 
 # Mostly from https://github.com/pytorch/examples/tree/master/super_resolution
 def download_bsd300(dest="image_data"):
@@ -32,26 +31,32 @@ def download_bsd300(dest="image_data"):
 
 def get_image_from_file(filename, crop_size=256):
     image = tf.io.read_file(filename)
-    image = tf.image.decode_jpeg(image)
+    image = tf.image.decode_png(image)
+    image= tf.cast(image, tf.float32)
     image_height = tf.shape(image)[0]
     image_width = tf.shape(image)[1]
     offset_height = (image_height-crop_size) // 2
     offset_width = (image_width-crop_size) // 2
     original_image = tf.image.crop_to_bounding_box(image, offset_height, offset_width, crop_size, crop_size)
     downsampled_image = tf.image.resize(original_image, [crop_size // 2, crop_size // 2])
-    return original_image, downsampled_image
+    # convert to 0~1 and change HWC to CHW 
+    # (because the network accepts single channel. 
+    # See model.py line 12 for better understanding.)
+    original_image = tf.transpose(original_image / 255.0, [2, 0, 1])
+    downsampled_image = tf.transpose(downsampled_image / 255.0, [2, 0, 1])
+    return downsampled_image, original_image
 
 def get_training_set(upscale_factor):
     root_dir = download_bsd300()
     train_dir = join(root_dir, "train")
-    names = Dataset.list_files(train_dir)
+    names = tf.data.Dataset.list_files(train_dir)
     images = names.map(get_image_from_file)
     return images
 
 def get_test_set(upscale_factor):
     root_dir = download_bsd300()
     test_dir = join(root_dir, "test")
-    names = Dataset.list_files(test_dir)
+    names = tf.data.Dataset.list_files(test_dir)
     images = names.map(get_image_from_file)
     return images
     
--- train.py
+++ train.py
@@ -1,11 +1,51 @@
 import argparse
 import tensorflow as tf
+from model import ESPCN
+from data import get_training_set, get_test_set
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [INFO] %(message)s')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-upscale_factor', default=2, type=int)
+parser.add_argument('-num_epochs', default=100, type=int)
+parser.add_argument('-batch_size', default=32, type=int)
+parser.add_argument('-seed', default=123, type=int)
+args = parser.parse_args()
+assert args.device >= 0
+tf.random.set_seed(args.seed)
+model = ESPCN(args.upscale_factor)
+
+# Dataset
+train_dataset = get_training_set(args.upscale_factor)
+test_dataset = get_test_set(args.upscale_factor)
+
+# Loss & optimizer
+loss_object = tf.keras.losses.MSE()
+optimizer = tf.keras.optimizers.Adam()
+
+# Keras metrics
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+test_loss = tf.keras.metrics.Mean(name='test_loss')
 
 
+def train_step(ds_image, image):
+    with tf.GradientTape() as tape:
+        generated_image = model(ds_image)
+        loss = loss_object(generated_image, image)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-upscale_factor', default=2, type=int)
-    parser.add_argument('-num_epochs', default=100, type=int)
-    args = parser.parse_args()
+    train_loss(loss)
 
+
+def test_step(ds_image, image):
+    generated_image = model(ds_image)
+    t_loss = loss_object(generated_image, image)
+    test_loss(t_loss)
+
+for epoch in range(args.num_epochs):
+    for ds_image, image in train_dataset:
+        train_step(ds_image, image)
+    for test_ds_image, test_image in test_dataset:
+        test_step(test_ds_image, test_image)
+    logging.info('epoch: %d, train_loss: %f, test_loss: %f' % (epoch+1, train_loss.result(), test_loss.result()))
