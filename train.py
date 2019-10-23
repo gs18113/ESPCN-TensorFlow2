a606daa9066fe634b98f8ed13000200fcf1da055
gs18113 <seungmin960@gmail.com>
2019-10-23 22:14:07 +0900

    init

--- train.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import tensorflow as tf
-
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-upscale_factor', default=2, type=int)
-    parser.add_argument('-num_epochs', default=100, type=int)
