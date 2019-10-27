d199d603df3698bba4f7829f88a6ab4ffea69f4c
gs18113 <seungmin960@gmail.com>
2019-10-27 13:32:39 +0900

    type & logging

--- tflite.py
+++ tflite.py
@@ -13,11 +13,13 @@ parser.add_argument('-saved_dir', default='saved_models', type=str)
 parser.add_argument('-tflite_dir', default='tflite_model', type=str)
 args = parser.parse_args()
 
-saved_path = join(join(args.save_dir, args.exp_name), str(args.model_epoch))
+saved_path = join(join(args.saved_dir, args.exp_name), str(args.model_epoch))
 assert exists(saved_path)
+logging.info('Creating converter from saved path %s...' % saved_path)
 converter = tf.lite.TFLiteConverter.from_saved_model(saved_path)
 tflite_model = converter.convert()
 if not exists(args.tflite_dir):
     os.makedirs(args.tflite_dir)
 
 open(join(args.tflite_dir, 'converted_model_'+args.exp_name+'_epoch_'+str(args.model_epoch)+'.tflite')).write(tflite_model)
+logging.info('Converting successful!')
