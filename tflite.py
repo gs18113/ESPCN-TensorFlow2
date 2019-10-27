109911f35d7e6d6943166ce3e6cbb0376a27f28c
gs18113 <seungmin960@gmail.com>
2019-10-27 13:29:47 +0900

    tflite

---
+++ tflite.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import argparse
+import logging
+import os 
+from os.path import join, exists
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [INFO] %(message)s')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-model_epoch', type=int, required=True)
+parser.add_argument('-exp_name', type=str, required=True)
+parser.add_argument('-saved_dir', default='saved_models', type=str)
+parser.add_argument('-tflite_dir', default='tflite_model', type=str)
+args = parser.parse_args()
+
+saved_path = join(join(args.save_dir, args.exp_name), str(args.model_epoch))
+assert exists(saved_path)
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_path)
+tflite_model = converter.convert()
+if not exists(args.tflite_dir):
+    os.makedirs(args.tflite_dir)
+
+open(join(args.tflite_dir, 'converted_model_'+args.exp_name+'_epoch_'+str(args.model_epoch)+'.tflite')).write(tflite_model)
